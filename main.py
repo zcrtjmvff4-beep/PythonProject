44bc4af52eebb650bb4083e111b09d5c54ffbba7
Sofia <sofii.05@mail.ru>
2026-02-06 12:21:12 +0300

    Расчет поездок

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
 china = 350_000
 dubai = 150_000
-bali_ = 400_000
+bali = 400_000
 
+dist = input("Введите пунк поездки (china, dubai, bali): ")
+count = int(input("Введите количество людей:  "))
+
+if dist == "china":
+    price = china * count
+elif dist == "dubai":
+    price = dubai * count
+else:
+    price = bali * count
+
+print("Цена поездки: ", price)
